b3ffc598b81ed0a56e4225d9ed61fc12f7afcf22
lucca3447 <lucca3447@hotmail.com>
2025-11-14 12:14:36 -0300

    Tralhando no menu

--- menu.py
+++ menu.py
@@ -6,4 +6,4 @@ import grafocidadesbahia
 
 print("Algoritmos de busca em grafo de cidades da bahia")
 print("Qual algoritmo de busca deseja aplicar?")
-print("1-Dijkstra \n Busca em Largura")
+print("1-Dijkstra \n 2-A* \n 3-BFS(Garante apenas o menor número de arestasm, não funciona por pesos)\n 4-DFS (Apneas vai percorrer o grafo de forma exploratoria)")
