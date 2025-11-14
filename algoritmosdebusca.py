96b9e75577f135e995d553c82480b77149e2bc4a
lucca3447 <lucca3447@hotmail.com>
2025-11-14 11:33:08 -0300

    Algoritmos corrigidos

--- algoritmosdebusca.py
+++ algoritmosdebusca.py
@@ -1,12 +1,14 @@
 import networkx as nx
 from grafocidadesbahia import G
 from grafocidadesbahia import coordenadas
-from grafocidadesbahia import plt
+import matplotlib.pyplot as plt
+
 
 def Dijkstra(G, origem, destino):
     menor_caminho = nx.shortest_path(G, source=origem, target=destino, weight="weight")
     distancia_total = nx.shortest_path_length(G, source=origem, target=destino, weight="weight")
     return menor_caminho, distancia_total
+
 
 def bfs(G, origem):
     visitados = []
@@ -16,23 +18,21 @@ def bfs(G, origem):
         atual = fila.pop(0)
         if atual not in visitados:
             visitados.append(atual)
-            fila.extend(G.vizinhos(atual))
+            fila.extend(list(G.neighbors(atual)))  
     return visitados
 
-def dfs(G,origem):
-    
+
+def dfs(G, origem):
     visitados = []
-    
+
     def cavar(no):
-        visitados.append(no)
-        for cavados in G.vizinhos(no):
-            if viz not in visitados:
+        if no not in visitados:        
+            visitados.append(no)
+            for viz in G.neighbors(no): 
                 cavar(viz)
 
     cavar(origem)
     return visitados
-
-
     
 
     
