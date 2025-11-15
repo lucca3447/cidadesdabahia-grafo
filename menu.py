5121dcf09ffb0871954fced11da6bc293c716b95
lucca3447 <lucca3447@hotmail.com>
2025-11-15 11:15:27 -0300

    Adicionado A*,funcional

--- menu.py
+++ menu.py
@@ -5,6 +5,8 @@ import os
 from algoritmosdebusca import Dijkstra
 from grafocidadesbahia import exibir_grafo 
 from grafocidadesbahia import G, coordenadas
+from algoritmosdebusca import heuristica
+from algoritmosdebusca import A_estrela
 
 while True:
     os.system("cls")
@@ -29,13 +31,26 @@ while True:
             
             print(f"Distância total aproximada entre {origem} e {destino}--> {distancia_total} km")
             print(f"Menor caminho percorrido segundo o algoritmo de Dijkstra--> {menor_caminho}")
-            input("\nAperte  espaço para  voltar ao continuar")
-        
+            input("\nAperte  espaço para  voltar ao menu")
+        case "3":
+            input("Você escolheu aplicar A*, aperte qualquer tecla para continuar")
+            print("A quais cidades (vertices) deseja aplicar o algoritmo?")
+            origem = input("Insira a origem ->")
+            destino = input("Insira o destino ->")
+
+            menor_caminho, distancia_total = A_estrela(G, origem, destino, coordenadas)
+
+            print(f"Distancia total aproximada entre {origem} e {destino} --> {distancia_total}")
+            print(f"Menor caminho encontrado segundo o algoritmo A* --> {menor_caminho}")
+            input("\nAperte espaço para voltar ao menu")
+
+
             
+
         case "6":
             confirmar = input("Você escolheu sair. Confirmar (s/n)? ")
             if confirmar.lower() == "s":
-                print("Encerrando o programa... até mais!")
+                print("Encerrando...")
                 break  
             else:
                 print("Retornando ao menu...")
